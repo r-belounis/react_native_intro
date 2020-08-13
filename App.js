1334fa5a001f4539eb05cdefc2118941cce0b813
rbelounis <65893991+r-belounis@users.noreply.github.com>
2020-08-13 18:37:57 +0200

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Navbar from './components/core/Navbar/Navbar'
+
+class App extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Navbar />
+        <Text>This is a test text</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +25,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
